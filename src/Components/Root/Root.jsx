21d02d5859cb75aee05a0fe675a4e5fc040e54fd
Hasnine670817 <hasninetorun@gmail.com>
2024-11-03 22:32:41 +0600

    footer-area done

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../Pages/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Footer/Footer';
 
 const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
